e3a5f86b1fcbf1ab34faabd5cd6208cca574442e
Miguel Migliorelli Bringhenti <miglenti@gmail.com>
2025-03-03 23:44:31 -0300

    created About Me section and its i18n

--- src/components/sections/AboutMe.vue
+++ src/components/sections/AboutMe.vue
@@ -1,3 +1,51 @@
+<script setup lang="ts">
+import { useI18n } from "vue-i18n";
+
+const { t } = useI18n();
+const techs = [
+  "Vue",
+  "React",
+  "React Native",
+  "Jetpack Compose",
+  "Express",
+  "Prisma",
+  "MongoDB",
+  "Nginx",
+  "Docker",
+];
+</script>
+
 <template>
-  <section id="aboutme" class="h-dvh">aboutme</section>
+  <section id="aboutme" class="py-18">
+    <div class="container mx-auto space-y-8 px-4">
+      <div class="flex gap-4 text-5xl" v-animate-on-view="">
+        <div class="size-[1em] md:hidden">
+          <img src="/migliorelli.png" class="size-full rounded-lg" />
+        </div>
+
+        <h1 class="font-bold text-white">
+          {{ t("aboutme.title") }}
+        </h1>
+      </div>
+
+      <div class="grid gap-8 md:grid-cols-3">
+        <div class="col-span-2 flex flex-col justify-center gap-8">
+          <p v-animate-on-view="{ delay: 100 }">
+            {{ t("aboutme.description") }}
+          </p>
+          <ul class="flex flex-wrap gap-4" v-animate-on-view="{ delay: 200 }">
+            <div v-for="tech in techs" class="ship">
+              {{ tech }}
+            </div>
+          </ul>
+        </div>
+
+        <div class="hidden justify-center md:flex" v-animate-on-view>
+          <div class="size-80">
+            <img src="/migliorelli.png" class="w-full rounded-lg" />
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
--- src/lib/i18n.ts
+++ src/lib/i18n.ts
@@ -18,11 +18,17 @@ const messages = {
       presentation: "Olá, eu me chamo",
       im: "Eu sou um",
       roles:
-        "Desenvolvedor Fullstack//Programador Mobile//Apaixonado por Tecnologia//Mestre em Resolver Desafios",
+        "Desenvolvedor Fullstack//Desenvolvedor MEVN/MERN//Programador Mobile//Apaixonado por Tecnologia",
       description:
         "Desenvolvedor web fullstack especializado em React, Vue, Express.js e Typescript. Projetos responsivos e soluções web modernas. ",
       resume: "Baixar currículo",
       getintouch: "Entrar em contato",
+    },
+
+    aboutme: {
+      title: "Sobre mim",
+      description:
+        "Comecei a programar aos 14 anos, criando minigames para o Transformice e mapas para o Roblox. Desde então, venho me aprofundando no universo da tecnologia, sempre com o foco em aprender coisas novas. Ao longo dos anos, desenvolvi desde sistemas internos de gerenciamento de empresas até aplicativos móveis. Atualmente, estou me especializando em segurança da informação, buscando me tornar um profissional referência nessa área. Atualmente, curso Técnico em Informática para a Internet no IFES Colatina, onde continuo expandindo meus conhecimentos. Estou sempre disposto a aprender novas tecnologias e ferramentas, e busco soluções eficientes para garantir a segurança e o melhor desempenho em meus projetos.",
     },
   },
   en: {
@@ -42,11 +48,17 @@ const messages = {
       presentation: "Hi, my name is",
       im: "I'm a",
       roles:
-        "Fullstack Developer//Mobile App Developer//Tech Enthusiast//Problem Solver",
+        "Fullstack Developer//MEVN/MERN Developer//Mobile App Developer//Tech Enthusiast",
       description:
         "Fullstack web developer specialized in React, Vue, Express.js and TypeScript. Responsive projects and modern web solutions.",
       resume: "Download resume",
       getintouch: "Get in touch",
+    },
+
+    aboutme: {
+      title: "About me",
+      description:
+        "I started programming at the age of 14, creating minigames for Transformice and maps for Roblox. Since then, I have been diving deeper into the world of technology, always focused on learning new things. Over the years, I have developed everything from internal management systems for companies to mobile applications. Currently, I am specializing in information security, aiming to become a reference professional in this field. I am currently enrolled in the Technical Course in Internet Informatics at IFES Colatina, where I continue to expand my knowledge. I am always willing to learn new technologies and tools, and I seek efficient solutions to ensure security and optimal performance in my projects.",
     },
   },
 };
