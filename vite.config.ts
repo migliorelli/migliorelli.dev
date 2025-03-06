8392bce9d8cdb9fa97f2ab60380dd4ff5f44eabd
Miguel Migliorelli Bringhenti <miglenti@gmail.com>
2025-03-05 22:14:44 -0300

    added sitemap

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,22 @@
 import tailwindcss from "@tailwindcss/vite";
 import vue from "@vitejs/plugin-vue";
 import { defineConfig } from "vite";
+import sitemap from "vite-plugin-sitemap";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), tailwindcss()],
+  plugins: [
+    vue(),
+    tailwindcss(),
+    sitemap({
+      hostname: "https://migliorelli.dev",
+      outDir: "dist",
+      i18n: {
+        defaultLanguage: "pt-BR",
+        languages: ["pt-BR", "en"],
+      },
+    }),
+  ],
   server: {
     port: 3000,
     strictPort: true,
