14769fbbdaa1f2d507c76005a36dfbf80d572c73
Miguel Migliorelli Bringhenti <miglenti@gmail.com>
2025-03-13 19:44:12 -0300

    added shadow-primary on hover

--- src/components/cards/ProjectCard.vue
+++ src/components/cards/ProjectCard.vue
@@ -17,34 +17,40 @@ const { t } = useI18n();
 </script>
 
 <template>
-  <div
-    class="group border-primary-950/70 flex flex-col gap-2 rounded-lg border bg-black p-4"
-    v-animate-on-view
-  >
-    <div class="aspect-video overflow-hidden rounded-lg">
-      <img
-        :src="image"
-        class="mx-auto h-full rounded-lg object-cover transition-transform duration-300 ease-in-out group-hover:scale-110"
-      />
-    </div>
-    <div class="flex flex-1 flex-col gap-4">
-      <p class="text-xl text-white">{{ title }}</p>
-
-      <p class="text-slate-400">{{ description }}</p>
-
-      <div class="flex flex-wrap gap-4">
-        <div v-for="tech in techs" :key="tech" class="chip px-2 py-1">
-          {{ tech }}
-        </div>
+  <div v-animate-on-view class="flex">
+    <div
+      class="group border-primary-950/70 hover:shadow-primary flex flex-col gap-2 rounded-lg border bg-black p-4 transition-shadow"
+    >
+      <div class="aspect-video overflow-hidden rounded-lg">
+        <img
+          :src="image"
+          class="mx-auto h-full rounded-lg object-cover transition-transform duration-300 ease-in-out group-hover:scale-110"
+        />
       </div>
+      <div class="flex flex-1 flex-col gap-4">
+        <p class="text-xl text-white">{{ title }}</p>
 
-      <div class="mt-auto flex gap-4">
-        <a :href="href" v-if="href" target="_blank" class="btn btn-border">
-          <ArrowUpRight :size="18" /> {{ t("projects.access") }}
-        </a>
-        <a :href="github" v-if="github" target="_blank" class="btn btn-border">
-          <Github :size="18" /> Github
-        </a>
+        <p class="text-slate-400">{{ description }}</p>
+
+        <div class="flex flex-wrap gap-4">
+          <div v-for="tech in techs" :key="tech" class="chip px-2 py-1">
+            {{ tech }}
+          </div>
+        </div>
+
+        <div class="mt-auto flex gap-4">
+          <a :href="href" v-if="href" target="_blank" class="btn btn-border">
+            <ArrowUpRight :size="18" /> {{ t("projects.access") }}
+          </a>
+          <a
+            :href="github"
+            v-if="github"
+            target="_blank"
+            class="btn btn-border"
+          >
+            <Github :size="18" /> Github
+          </a>
+        </div>
       </div>
     </div>
   </div>
