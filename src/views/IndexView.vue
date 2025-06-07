9effc896725da56d85fd9921b1d4c4ab33f027f0
Miguel Migliorelli Bringhenti <miglenti@gmail.com>
2025-06-07 09:53:30 -0300

    vercel analytics

--- src/views/IndexView.vue
+++ src/views/IndexView.vue
@@ -6,10 +6,12 @@ import Contact from "../components/sections/Contact.vue";
 import Hero from "../components/sections/Hero.vue";
 import Projects from "../components/sections/Projects.vue";
 // import Services from "../components/sections/Services.vue";
+import { Analytics } from "@vercel/analytics/vue";
 import Skills from "../components/sections/Skills.vue";
 </script>
 
 <template>
+  <Analytics />
   <Navbar />
   <main class="inset-0 h-full w-full">
     <Hero />
